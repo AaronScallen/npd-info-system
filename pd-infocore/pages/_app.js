78023d058a942f630e55aee582b33e52bddd84f1
AaronScallen <aaron.scallen@nisd.net>
2021-01-13 13:24:02 -0600

    initial commit

---
+++ pd-infocore/pages/_app.js
@@ -0,0 +1,9 @@
+import { AuthProvider } from '../utils/auth'
+const App = ({ Component, pageProps }) => {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
+}
+export default App
